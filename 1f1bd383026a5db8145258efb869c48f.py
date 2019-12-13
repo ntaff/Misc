f89be3e056ae0ec74c7cf7f5e17fd154be18f5f4
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2019-12-13 13:43:55 +0100

    Create 1f1bd383026a5db8145258efb869c48f.py

---
+++ 1f1bd383026a5db8145258efb869c48f.py
@@ -0,0 +1,30 @@
+def decrypt(l):
+  key = l[:2]
+  encr = l[2:]
+  l=[]
+  if (len(encr)%2) == 0:
+    tailleencr = len(encr)/2
+  else:
+    tailleencr = (len(encr)/2)+1
+  tailleencr = int(tailleencr)
+  for i in range(tailleencr):
+    l.append(encr[(i*2):(i*2+2)])
+  l2=[]
+  key2 = int(key,16)
+  for i in range (len(l)):
+    encr2 = int(l[i],16)
+    decr = encr2 ^ key2
+    azerty = str(chr(decr))
+    l2.append(azerty)
+  return l2
+
+file = open('encrypted.txt', 'r')
+lll=[]
+for line in file.readlines():
+
+  line = line.rstrip()
+  liste = decrypt(line)
+  makeitastring = ''.join(map(str, liste))
+  lll.append(makeitastring)
+resultat = ''.join(map(str, lll))
+print(resultat)
